cd96d0491443badc62172595db1fd3b197f9d532
golebiowskib <bartosz.golebiowski@@ttpsc.pl>
2021-11-18 18:17:22 +0100

    add subscription notification alert

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,27 @@
+import { useEffect } from "react";
 import BasicLayout from "../components/basic-layout";
 import "../styles/globals.css";
 
+const useSubscriptionNotification = () => {
+  useEffect(() => {
+    !(function (c, h, i, m, p) {
+      (m = c.createElement(h)),
+        (p = c.getElementsByTagName(h)[0]),
+        (m.async = 1),
+        (m.src = i),
+        p.parentNode.insertBefore(m, p);
+    })(
+      document,
+      "script",
+      "https://chimpstatic.com/mcjs-connected/js/users/c041361b6d107f67adf13f133/ab2edfe93295f5d932d0f26bd.js"
+    );
+    
+  }, []);
+};
+
 export default function MyApp({ Component, pageProps }) {
   const getLayout = Component.getLayout ?? ((page) => BasicLayout(page));
+  useSubscriptionNotification();
 
   return getLayout(<Component {...pageProps} />);
 }
